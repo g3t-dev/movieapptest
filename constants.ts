949a5d2ec98acdda1ede0ccee98f3b33e835bfa5
g3t-dev <g3t2010@gmail.com>
2026-05-08 20:52:28 +0100

    Replace folder contents

--- constants.ts
+++
@@ -1,4 +0,0 @@
-const API_URL = "https://api.themoviedb.org/3";
-const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
-
-export { API_URL, IMAGE_PATH };
--- types/movie.d.ts
+++
@@ -1,8 +0,0 @@
-interface IMovie {
-  title: string;
-  poster_path: string;
-  id: number;
-  vote_average: number;
-  overview: string;
-  release_date: string;
-}
